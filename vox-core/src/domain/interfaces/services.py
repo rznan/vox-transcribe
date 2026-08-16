cdb3b9d073f82ed67cd99f1231b2f7ff19ff0482
rznan <sc.renan7@gmail.com>
2026-08-16 20:10:23 -0300

    creates service interface for tasks & batches

---
+++ vox-core/src/domain/interfaces/services.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+
+from typing import Generic, List, Sequence, TypeVar, Optional
+
+from src.domain.entities import Batch, Task, Worker, TaskStatus
+
+
+class TaskService(ABC):
+    @abstractmethod
+    async def create_batch(self, new_batch: Batch) -> Batch:
+        """Registra um batch novo no repositório e o agenda no task_scheduler"""
+        pass
+
+    @abstractmethod
+    async def delete_batch(self, batch_id: int) -> None:
+        """
+        Remove os dados do batch com id específicado.
+        Cancela qualquer execução em andamento antes de deletar
+        """
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, task_id: int) -> Task | None:
+        """
+        Retorna a task completa (com tentativas e resultado) solicitada
+        """
+        pass
+
+    @abstractmethod
+    async def list_batches(self, limit: int = 100, offset: int = 0) -> List[Batch]:
+        """
+        Lista os batches armazenados em memória
+        """
+        pass
